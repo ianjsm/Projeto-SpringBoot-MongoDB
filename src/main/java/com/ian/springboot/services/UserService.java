efd0838bd971b6f5a7eb42ca554d9eb2ab371624
Ian Soares <ian.soares@aluno.uece.br>
2024-03-20 14:01:07 -0300

    Exception handler

--- src/main/java/com/ian/springboot/services/UserService.java
+++ src/main/java/com/ian/springboot/services/UserService.java
@@ -1,12 +1,14 @@
 package com.ian.springboot.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ian.springboot.domain.User;
 import com.ian.springboot.repository.UserRepository;
+import com.ian.springboot.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	 
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 }
