dacb0a1b9130447e233d2e2a0f18f09d01835cc7
Ian Soares <ian.soares@aluno.uece.br>
2024-03-20 14:30:29 -0300

    User insertion

--- src/main/java/com/ian/springboot/services/UserService.java
+++ src/main/java/com/ian/springboot/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ian.springboot.domain.User;
+import com.ian.springboot.dto.UserDTO;
 import com.ian.springboot.repository.UserRepository;
 import com.ian.springboot.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDto(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 }
