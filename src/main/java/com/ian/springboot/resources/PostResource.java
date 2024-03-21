bce35fffae3a714093984e9e42409bb9b8829665
Ian Soares <ian.soares@aluno.uece.br>
2024-03-21 18:36:17 -0300

    Search methods

--- src/main/java/com/ian/springboot/resources/PostResource.java
+++ src/main/java/com/ian/springboot/resources/PostResource.java
@@ -1,25 +1,37 @@
 package com.ian.springboot.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ian.springboot.domain.Post;
+import com.ian.springboot.resources.util.URL;
 import com.ian.springboot.services.PostService;
 
 @RestController
 @RequestMapping(value = "/posts")
 public class PostResource {
-	
+
 	@Autowired
 	private PostService postService;
-	
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = postService.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@GetMapping(value = "/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		List<Post> posts = postService.findByTitle(text);
+		return ResponseEntity.ok().body(posts);
 	}
 }
--- src/main/java/com/ian/springboot/services/PostService.java
+++ src/main/java/com/ian/springboot/services/PostService.java
@@ -1,5 +1,6 @@
 package com.ian.springboot.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,4 +21,7 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 
+	public List<Post> findByTitle(String text){
+		return postRepository.findByTitleContainingIgnoreCase(text);
+	}
 }
